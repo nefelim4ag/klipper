c4097c85b98275f19f4cd997f7a7917ed900a9c5
Timofey Titovets <nefelim4ag@gmail.com>
2024-10-04 00:57:51 +0200

    adc_temperature: allow set report interval
    Signed-off-by: Timofey Titovets <nefelim4ag@gmail.com>

--- klippy/extras/adc_temperature.py
+++ klippy/extras/adc_temperature.py
@@ -12,7 +12,6 @@ import logging, bisect
 
 SAMPLE_TIME = 0.001
 SAMPLE_COUNT = 8
-REPORT_TIME = 0.300
 RANGE_CHECK_COUNT = 4
 
 # Interface between ADC and heater temperature callbacks
@@ -21,13 +20,15 @@ class PrinterADCtoTemperature:
         self.adc_convert = adc_convert
         ppins = config.get_printer().lookup_object('pins')
         self.mcu_adc = ppins.setup_pin('adc', config.get('sensor_pin'))
-        self.mcu_adc.setup_adc_callback(REPORT_TIME, self.adc_callback)
+        self.report_time = config.getfloat('sensor_interval', 0.3,
+                                           minval=0.1, maxval=0.3)
+        self.mcu_adc.setup_adc_callback(self.report_time, self.adc_callback)
         self.diag_helper = HelperTemperatureDiagnostics(
             config, self.mcu_adc, adc_convert.calc_temp)
     def setup_callback(self, temperature_callback):
         self.temperature_callback = temperature_callback
     def get_report_time_delta(self):
-        return REPORT_TIME
+        return self.report_time
     def adc_callback(self, read_time, read_value):
         temp = self.adc_convert.calc_temp(read_value)
         self.temperature_callback(read_time + SAMPLE_COUNT * SAMPLE_TIME, temp)
