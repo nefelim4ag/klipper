0682a49dc062393a0d1d4644cdd180a4a22aeda5
Timofey Titovets <nefelim4ag@gmail.com>
2024-12-09 00:34:05 +0100

    Hmm

--- klippy/extras/angle.py
+++ klippy/extras/angle.py
@@ -894,6 +894,7 @@ class AngleTMCCalibration:
         pos_angle = self.last_move_angle()
         # Try converge to ideal steps
         ideal_angle = pos_angle
+        pos_angle_prev = pos_angle
         logging.info(f"pos: {1024-self.mscnt_min}, tgt: {ideal_angle:.3f}, act: {pos_angle:.3f}, fs_angle: {fs_angles[0]:3.3f}")
         ideal_angle += self.ms_angle[0] * self.angle_dir
         min_dist = 1
@@ -907,9 +908,11 @@ class AngleTMCCalibration:
             self.move(-self.dir * self.step_dist * 2)
             self.move(self.dir * self.step_dist)
             pos_angle = self.last_move_angle()
+            pos_diff = self.angle_dist(pos_angle, pos_angle_prev)
+            pos_angle_prev = pos_angle
             distance = self.angle_dist(ideal_angle, pos_angle)
             ms_dist.append(distance)
-            logging.info(f"pos: {pos:4}, tgt: {ideal_angle:3.3f}, act: {pos_angle:3.3f}, dist: {distance:1.3f}, tgt_fs_angle: {fs_angles[1+pos//256]:3.3f}")
+            logging.info(f"pos: {pos:4}, tgt: {ideal_angle:3.3f}, act: {pos_angle:3.3f}, dist: {distance:1.3f}, diff: {pos_diff:3.3f}, tgt_fs_angle: {fs_angles[1+pos//256]:3.3f}")
             ideal_angle += self.ms_angle[pos//256] * self.angle_dir
             if (pos % 256) == (256 - self.mscnt_min):
                 # Compensate error
