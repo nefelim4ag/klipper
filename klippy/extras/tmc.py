9e6d570b75d364c203b778dd137a1f8ef8f3bc40
Timofey Titovets <nefelim4ag@gmail.com>
2025-04-19 15:28:46 +0200

    tmc: pass cached field values to tmc status

--- klippy/extras/tmc.py
+++ klippy/extras/tmc.py
@@ -216,6 +216,7 @@ class TMCCommandHelper:
         self.current_helper = current_helper
         self.echeck_helper = TMCErrorCheck(config, mcu_tmc)
         self.fields = mcu_tmc.get_fields()
+        self.fields_cache = {}
         self.read_registers = self.read_translate = None
         self.toff = None
         self.mcu_phase_offset = None
@@ -399,7 +400,15 @@ class TMCCommandHelper:
                'run_current': current[0],
                'hold_current': current[1]}
         res.update(self.echeck_helper.get_status(eventtime))
+        if res["drv_status"]:
+            res["drv_status"].update(self.fields_cache)
+        else:
+            res["drv_status"] = self.fields_cache
         return res
+    def _fcache_update(self, reg, val):
+        fields = self.fields.get_reg_fields(reg, val)
+        for field, v in fields.items():
+            self.fields_cache[field] = v
     # DUMP_TMC support
     def setup_register_dump(self, read_registers, read_translate=None):
         self.read_registers = read_registers
@@ -418,12 +427,14 @@ class TMCCommandHelper:
             if (val is not None) and (reg_name not in self.read_registers):
                 # write-only register
                 gcmd.respond_info(self.fields.pretty_format(reg_name, val))
+                self._fcache_update(reg_name, val)
             elif reg_name in self.read_registers:
                 # readable register
                 val = self.mcu_tmc.get_register(reg_name)
                 if self.read_translate is not None:
                     reg_name, val = self.read_translate(reg_name, val)
                 gcmd.respond_info(self.fields.pretty_format(reg_name, val))
+                self._fcache_update(reg_name, val)
             else:
                 raise gcmd.error("Unknown register name '%s'" % (reg_name))
         else:
@@ -431,9 +442,11 @@ class TMCCommandHelper:
             for reg_name, val in self.fields.registers.items():
                 if reg_name not in self.read_registers:
                     gcmd.respond_info(self.fields.pretty_format(reg_name, val))
+                    self._fcache_update(reg_name, val)
             gcmd.respond_info("========== Queried registers ==========")
             for reg_name in self.read_registers:
                 val = self.mcu_tmc.get_register(reg_name)
+                self._fcache_update(reg_name, val)
                 if self.read_translate is not None:
                     reg_name, val = self.read_translate(reg_name, val)
                 gcmd.respond_info(self.fields.pretty_format(reg_name, val))
