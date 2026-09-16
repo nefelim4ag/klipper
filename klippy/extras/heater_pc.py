62620496173fef169fa96d9b399590ff8971d18e
Timofey Titovets <nefelim4ag@gmail.com>
2026-09-17 01:26:33 +0200

    tmp

--- klippy/extras/heater_pc.py
+++ klippy/extras/heater_pc.py
@@ -12,8 +12,6 @@ class HeaterPredictControl:
     def __init__(self, config):
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
-        self.config = config
-        self.eval_time = 0.3
         self.min_pwm = -1.0
         self.max_pwm = 1.0
         self.old_control = None
@@ -21,6 +19,7 @@ class HeaterPredictControl:
         if len(name_parts) != 2:
             raise config.error("Section name '%s' is not valid"
                                % (config.get_name(),))
+        self.heater_name = config.get_name().split()[-1]
         # Use lock to pass data to/from heater code
         self.lock = threading.Lock()
         self.output = .0
@@ -32,14 +31,13 @@ class HeaterPredictControl:
         self.printer.register_event_handler("klippy:connect",
                                             self.handle_connect)
     def handle_connect(self):
-        pheaters = self.printer.load_object(self.config, 'heaters')
-        heater_name = self.config.get_name().split()[-1]
-        heater = pheaters.heaters.get(heater_name)
+        pheaters = self.printer.lookup_object('heaters')
+        heater = pheaters.heaters.get(self.heater_name)
         if heater is None:
-            self.config.error("Heater %s is not registered" % (heater_name))
-        self.eval_time = heater.get_pwm_delay()
-        self.min_pwm = -heater.get_max_power()
+            raise self.printer.config_error("Heater %s is not registered" % (
+                self.heater_name))
         self.max_pwm = heater.get_max_power()
+        self.min_pwm = -self.max_pwm
         self.old_control = heater.set_control(self)
         gcode = self.printer.lookup_object('gcode')
         params = {"TEMPLATE": self.template_name}
